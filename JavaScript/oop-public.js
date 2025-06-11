9943e2be12f9977ad2ba5dbf46ad47d70dd6b623
Alexandrus Mechanicus <sologub.sasha2001@gmail.com>
2025-06-11 15:14:58 +0300

    Possible race condition in code

--- JavaScript/oop-public.js
+++ JavaScript/oop-public.js
@@ -5,25 +5,78 @@ class Point {
     this.x = x;
     this.y = y;
   }
-
+  
   move(x, y) {
     this.x += x;
     this.y += y;
   }
-
+  
   clone() {
     return new Point(this.x, this.y);
   }
-
+  
   toString() {
     return `(${this.x}, ${this.y})`;
   }
 }
 
-// Usage
+console.log('\nRC1');
 
-const p1 = new Point(10, 20);
-console.log(p1.toString());
-const c1 = p1.clone();
-c1.move(-5, 10);
-console.log(c1.toString());
+const sharedPoint = new Point(0, 0);
+console.log('Start position:', sharedPoint.toString());
+
+function simulateRaceCondition() {
+  const process1 = () => {
+    const currentX = sharedPoint.x; 
+    const currentY = sharedPoint.y; 
+
+    setTimeout(() => {
+      sharedPoint.x = currentX + 10;
+      sharedPoint.y = currentY + 10;
+      console.log('Proccess 1 end:', sharedPoint.toString());
+    }, 1);
+  };
+
+  const process2 = () => {
+    const currentX = sharedPoint.x; 
+    const currentY = sharedPoint.y; 
+    
+    setTimeout(() => {
+      sharedPoint.x = currentX + 5;
+      sharedPoint.y = currentY + 5;
+      console.log('Proccess 2 end:', sharedPoint.toString());
+    }, 1);
+  };
+  
+  process1();
+  process2();
+  
+  setTimeout(() => {
+    console.log('Final result:', sharedPoint.toString());
+  }, 50);
+}
+
+simulateRaceCondition();
+
+setTimeout(() => {
+  console.log('\nRC2');
+  
+  const point = new Point(0, 0);
+  console.log('Start position:', point.toString());
+  
+  const promises = [];
+  
+  for (let i = 0; i < 5; i++) {
+    promises.push(new Promise(resolve => {
+      setTimeout(() => {
+        point.move(1, 1);
+        console.log(`Operation ${i + 1}:`, point.toString());
+        resolve();
+      }, Math.random() * 20);
+    }));
+  }
+  
+  Promise.all(promises).then(() => {
+    console.log('Final:', point.toString());
+  });
+}, 50);
